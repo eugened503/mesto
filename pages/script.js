2a8bc4fec26cba4f42c57f8d7f72a209ce230d31
Eugen Larionov <zhenia.larionoff2015@yandex.ru>
2020-04-16 23:46:17 +0300

    new bug fix

--- pages/script.js
+++ pages/script.js
@@ -1,39 +1,30 @@
-const openBtn = document.querySelector('.profile-info__button'); /*присвоение переменной элемента - кнопки редактирования формы*/
-const closeBtn = document.querySelector('.popup__close-button'); /*присвоение переменной элемента - кнопки закрытия формы*/
-const saveBtn = document.querySelector('.popup__btn'); /*присвоение переменной элемента - кнопки сохранения изменений формы*/
-const formElement = document.querySelector('.popup'); /*присвоение переменной элемента - формы*/
-const infoTitle = document.querySelector('.profile-info__title'); /*присвоение переменной элемента - "имя пользователя"*/
-const infoSubtitle = document.querySelector('.profile-info__subtitle'); /*присвоение переменной элемента - "род деятельности пользователя"*/
+const openBtn = document.querySelector('.profile-info__button'); //присвоение переменной элемента - кнопки редактирования формы
+const closeBtn = document.querySelector('.popup__close-button'); // присвоение переменной элемента - кнопки закрытия формы 
+const formElement = document.querySelector('.popup'); //присвоение переменной элемента - формы
+const infoTitle = document.querySelector('.profile-info__title'); //присвоение переменной элемента - "имя пользователя"
+const infoSubtitle = document.querySelector('.profile-info__subtitle'); //присвоение переменной элемента - "род деятельности пользователя"
+const saveBtn = document.querySelector('.popup__btn'); // присвоение переменной элемента - кнопки сохранения формы 
+const nameInput = document.querySelector('.popup__text_text-margin'); //присвоение переменной элемента - "поле ввода имени пользователя"
+const jobInput = document.querySelector('.popup__text_work-margin'); //присвоение переменной элемента - "поле ввода рода деятельности пользователя"
 
-openBtn.addEventListener('click', double); /*вызов функции открытия и закрытия формы*/
-closeBtn.addEventListener('click', double);
+function openPopup() { //функция открытия формы
+    formElement.classList.add('popup_opened');
+    nameInput.value = infoTitle.textContent;
+    jobInput.value = infoSubtitle.textContent;
+};
 
-function double() { /*функция открытия и закрытия формы*/
-    if (formElement.classList.contains('popup_opened')) {
-        document.querySelector('.popup').classList.remove('popup_opened'); /*закрытие формы*/
+function closePopup() { // функция закрытия формы
+    formElement.classList.remove('popup_opened')
+};
 
-        function clean() { /*функция сброса несохраненных данных формы*/
-            document.querySelector('.popup__text_text-margin').value = "Жак-Ив Кусто";
-            document.querySelector('.popup__text_work-margin').value = "Исследователь океана";
-        }
-        clean();
-    } else {
-        document.querySelector('.popup').classList.add('popup_opened'); /*открытие формы*/
-    }
-}
-saveBtn.addEventListener("click", function() { /*функция сохранения данных формы*/
-    document.querySelector('.popup').classList.remove('popup_opened');
-
-});
-
-function formSubmitHandler(evt) { /*функция отправки данных формы*/
+function formSubmitHandler(evt) { // функция отправки формы
     evt.preventDefault();
-
-    const nameInput = document.querySelector('.popup__text_text-margin').value; /*значение полей из свойства value*/
-    const jobInput = document.querySelector('.popup__text_work-margin').value;
-
-    infoTitle.textContent = nameInput; /*вставляем новые значения с помощью textContent*/
-    infoSubtitle.textContent = jobInput;
+    nameInput.getAttribute('value');
+    jobInput.getAttribute('value');
+    infoTitle.textContent = nameInput.value;
+    infoSubtitle.textContent = jobInput.value;
+    closePopup();
 }
-
-formElement.addEventListener('submit', formSubmitHandler);
+closeBtn.addEventListener('click', closePopup); // слушатель события кнопки закрытия формы
+openBtn.addEventListener('click', openPopup); // слушатель события кнопки открытия формы
+saveBtn.addEventListener('click', formSubmitHandler); //слушатель события кнопки отправки формы
